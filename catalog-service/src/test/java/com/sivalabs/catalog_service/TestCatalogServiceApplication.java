829fabddb80c212865556c30ffcefa54d36e6736
Bachir A. Bachir (BCX) <amidbach@hotmail.com>
2026-06-27 23:06:37 +0200

    SivaLabs :: [catalog-service] Add {spotless-maven-plugin} dependency  --> Spotless Code Formatter: Keep your code spotless ((https://github.com/diffplug/spotless))

--- catalog-service/src/test/java/com/sivalabs/catalog_service/TestCatalogServiceApplication.java
+++ catalog-service/src/test/java/com/sivalabs/catalog_service/TestCatalogServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 public class TestCatalogServiceApplication {
 
 	public static void main(String[] args) {
+		// spotless:off
 		SpringApplication
 				.from(CatalogServiceApplication::main)
 				.with(TestcontainersConfiguration.class)
 				.run(args);
+		// spotless:on
 	}
 
 }
